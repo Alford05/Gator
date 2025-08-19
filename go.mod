06e69fe127c55c1736c8843566dda3e3947e6962
Oakley <dalford278@gmail.com>
2025-08-18 21:00:04 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Alford05/Gator
+
+go 1.23.5
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Alford05/Gator/internal/config"
+)
+
+func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("Read config: %+v\n", cfg)
+
+	err = cfg.SetUser("Daniel")
+	if err != nil {
+		log.Fatalf("couldn't set current user: %v", err)
+	}
+
+	cfg, err = config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("Read config again: %+v\n", cfg)
+}
+
+
+
