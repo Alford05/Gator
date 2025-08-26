34756fd8eed5129dac80f0523ebd29f4b3af89ce
Oakley <dalford278@gmail.com>
2025-08-25 21:54:47 -0400

    added posts function and browse command

---
+++ handler_reset.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"context"
+)
+
+func handlerReset(s *state, cmd command) error {
+	err := s.db.DeleteUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("could not delete users: %w", err)
+	}
+	fmt.Println("Database reset successfully!")
+	return nil
+}
+
--- main.go
+++ main.go
@@ -1,30 +1,76 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"os"
+	"database/sql"
+	"context"
+	_ "github.com/lib/pq"
 
 	"github.com/Alford05/Gator/internal/config"
+	"github.com/Alford05/Gator/internal/database"
 )
+
+type state struct {
+	db *database.Queries
+	cfg *config.Config
+}
 
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
-	fmt.Printf("Read config: %+v\n", cfg)
-
-	err = cfg.SetUser("Daniel")
+	
+	db, err := sql.Open("postgres", cfg.DBURL)
 	if err != nil {
-		log.Fatalf("couldn't set current user: %v", err)
+		log.Fatalf("error opening db: %v", err)
+	}
+	defer db.Close()
+	dbQueries := database.New(db)
+	s := &state{
+		cfg: &cfg,
+		db: dbQueries,
 	}
 
-	cfg, err = config.Read()
-	if err != nil {
-		log.Fatalf("error reading config: %v", err)
+	cmds := commands{
+		registeredCommands: make(map[string]func(*state, command) error),
 	}
-	fmt.Printf("Read config again: %+v\n", cfg)
+
+	cmds.register("register", handlerRegister)
+	cmds.register("login", handlerLogin)
+	cmds.register("reset", handlerReset)
+	cmds.register("users", handlerListUsers)
+	cmds.register("agg", handlerAgg)
+	cmds.register("addfeed", middlewareLoggedIn(handlerAddFeed))
+	cmds.register("feeds", handlerListFeeds)
+	cmds.register("follow", middlewareLoggedIn(handlerFollow))
+	cmds.register("following", middlewareLoggedIn(handlerListFeedFollows))
+	cmds.register("unfollow", middlewareLoggedIn(handlerUnfollow))
+	cmds.register("browse", middlewareLoggedIn(handlerBrowse))
+
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: CLI <commands> [args...]")
+	}
+	cmdName := os.Args[1]
+	cmdArgs := os.Args[2:]
+
+	err = cmds.run(s, command{Name: cmdName, Args: cmdArgs})
+	if err != nil {
+		log.Fatalf("error running command: %v", err)
+	}
 }
+
+func middlewareLoggedIn(handler func(s *state, cmd command, user database.User) error) func(*state, command) error {
+	return func(s *state, cmd command) error {
+		user, err := s.db.GetUser(context.Background(), s.cfg.CurrentUserName)
+		if err != nil {
+			return err
+		}
+		return handler(s, cmd, user)
+	}
+}
+
 
 
 
